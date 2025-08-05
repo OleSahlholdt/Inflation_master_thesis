f9d7c88cc16d190381c3150ed3732b2e007529c5
OleSahlholdt <o.sahlholdt@gmail.com>
2025-08-05 12:55:15 +0200

    Cleanup and prepare, do not transform preds

---
+++ FEDFormer/default_args.py
@@ -0,0 +1,240 @@
+import argparse
+import os
+import sys
+import torch
+import random
+import numpy as np
+from utils.tools import dotdict
+
+
+fix_seed = 2021
+random.seed(fix_seed)
+torch.manual_seed(fix_seed)
+np.random.seed(fix_seed)
+
+def FEDFormer_default_args():
+    parser = argparse.ArgumentParser(description='Autoformer & Transformer family for Time Series Forecasting')
+
+    # basic config
+    parser.add_argument('--is_training', type=int, default=1, help='status')
+    parser.add_argument('--task_id', type=str, default='test', help='task id')
+    parser.add_argument('--model', type=str, default='FEDformer',
+                        help='model name, options: [FEDformer, Autoformer, Informer, Transformer]')
+
+    # supplementary config for FEDformer model
+    parser.add_argument('--version', type=str, default='Fourier',
+                        help='for FEDformer, there are two versions to choose, options: [Fourier, Wavelets]')
+    parser.add_argument('--mode_select', type=str, default='random',
+                        help='for FEDformer, there are two mode selection method, options: [random, low]')
+    parser.add_argument('--modes', type=int, default=64, help='modes to be selected random 64')
+    parser.add_argument('--L', type=int, default=3, help='ignore level')
+    parser.add_argument('--base', type=str, default='legendre', help='mwt base')
+    parser.add_argument('--cross_activation', type=str, default='tanh',
+                        help='mwt cross atention activation function tanh or softmax')
+
+    # data loader
+    parser.add_argument('--data', type=str, default='custom', help='dataset type')
+    parser.add_argument('--root_path', type=str, default='./dataset/', help='root path of the data file')
+    parser.add_argument('--data_path', type=str, default='Inflation.csv', help='data file')
+    parser.add_argument('--features', type=str, default='M',
+                        help='forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, '
+                                'S:univariate predict univariate, MS:multivariate predict univariate')
+    parser.add_argument('--target', type=str, default='Austria', help='target feature in S or MS task')
+    parser.add_argument('--freq', type=str, default='m',
+                        help='freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, '
+                                'b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h')
+    parser.add_argument('--detail_freq', type=str, default='h', help='like freq, but use in predict')
+    parser.add_argument('--checkpoints', type=str, default='./FEDFormer_checkpoints/', help='location of model checkpoints')
+
+    # forecasting task
+    parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
+    parser.add_argument('--label_len', type=int, default=48, help='start token length')
+    parser.add_argument('--pred_len', type=int, default=96, help='prediction sequence length')
+    # parser.add_argument('--cross_activation', type=str, default='tanh'
+
+    # model define
+    parser.add_argument('--enc_in', type=int, default=92, help='encoder input size')
+    parser.add_argument('--dec_in', type=int, default=92, help='decoder input size')
+    parser.add_argument('--c_out', type=int, default=92, help='output size')
+    parser.add_argument('--d_model', type=int, default=512, help='dimension of model')
+    parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
+    parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
+    parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
+    parser.add_argument('--d_ff', type=int, default=2048, help='dimension of fcn')
+    parser.add_argument('--moving_avg', default=[24], help='window size of moving average')
+    parser.add_argument('--factor', type=int, default=1, help='attn factor')
+    parser.add_argument('--distil', action='store_false',
+                        help='whether to use distilling in encoder, using this argument means not using distilling',
+                        default=True)
+    parser.add_argument('--dropout', type=float, default=0.05, help='dropout')
+    parser.add_argument('--embed', type=str, default='timeF',
+                        help='time features encoding, options:[timeF, fixed, learned]')
+    parser.add_argument('--activation', type=str, default='gelu', help='activation')
+    parser.add_argument('--output_attention', action='store_true', help='whether to output attention in ecoder')
+    parser.add_argument('--do_predict', action='store_true', help='whether to predict unseen future data')
+
+    # optimization
+    parser.add_argument('--num_workers', type=int, default=10, help='data loader num workers')
+    parser.add_argument('--itr', type=int, default=3, help='experiments times')
+    parser.add_argument('--train_epochs', type=int, default=10, help='train epochs')
+    parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
+    parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
+    parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
+    parser.add_argument('--des', type=str, default='test', help='exp description')
+    parser.add_argument('--loss', type=str, default='mse', help='loss function')
+    parser.add_argument('--lradj', type=str, default='type1', help='adjust learning rate')
+    parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
+
+    # GPU
+    parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
+    parser.add_argument('--gpu', type=int, default=0, help='gpu')
+    parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
+    parser.add_argument('--devices', type=str, default='0,1', help='device ids of multi gpus')
+
+    args = dotdict(vars(parser.parse_args()))
+    return args
+
+def Informer_default_args():
+    parser = argparse.ArgumentParser(description='[Informer] Long Sequences Forecasting')
+
+    parser.add_argument('--model', type=str, default='informer',help='model of experiment, options: [informer, informerstack, informerlight(TBD)]')
+
+    # data loader
+    parser.add_argument('--data', type=str, default='custom', help='dataset type')
+    parser.add_argument('--root_path', type=str, default='./dataset/', help='root path of the data file')
+    parser.add_argument('--data_path', type=str, default='Inflation.csv', help='data file')
+    parser.add_argument('--features', type=str, default='M',
+                        help='forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, '
+                                'S:univariate predict univariate, MS:multivariate predict univariate')
+    parser.add_argument('--target', type=str, default='Austria', help='target feature in S or MS task')
+    parser.add_argument('--freq', type=str, default='m',
+                        help='freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, '
+                                'b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h')
+    parser.add_argument('--detail_freq', type=str, default='m', help='like freq, but use in predict')
+    parser.add_argument('--checkpoints', type=str, default='./Informer_checkpoints/', help='location of model checkpoints')
+
+    # forecasting task
+    parser.add_argument('--seq_len', type=int, default=96, help='input sequence length of Informer encoder')
+    parser.add_argument('--label_len', type=int, default=48, help='start token length of Informer decoder')
+    parser.add_argument('--pred_len', type=int, default=24, help='prediction sequence length')
+    
+    # Informer decoder input: concat[start token series(label_len), zero padding series(pred_len)]
+
+    parser.add_argument('--enc_in', type=int, default=92, help='encoder input size')
+    parser.add_argument('--dec_in', type=int, default=92, help='decoder input size')
+    parser.add_argument('--c_out', type=int, default=92, help='output size')
+    parser.add_argument('--d_model', type=int, default=512, help='dimension of model')
+    parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
+    parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
+    parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
+    parser.add_argument('--s_layers', type=str, default='3,2,1', help='num of stack encoder layers')
+    parser.add_argument('--d_ff', type=int, default=2048, help='dimension of fcn')
+    parser.add_argument('--factor', type=int, default=5, help='probsparse attn factor')
+    parser.add_argument('--padding', type=int, default=0, help='padding type')
+    parser.add_argument('--distil', action='store_false', help='whether to use distilling in encoder, using this argument means not using distilling', default=True)
+    parser.add_argument('--dropout', type=float, default=0.05, help='dropout')
+    parser.add_argument('--attn', type=str, default='prob', help='attention used in encoder, options:[prob, full]')
+    parser.add_argument('--embed', type=str, default='timeF', help='time features encoding, options:[timeF, fixed, learned]')
+    parser.add_argument('--activation', type=str, default='gelu',help='activation')
+    parser.add_argument('--output_attention', action='store_true', help='whether to output attention in ecoder')
+    parser.add_argument('--do_predict', action='store_true', help='whether to predict unseen future data')
+    parser.add_argument('--mix', action='store_false', help='use mix attention in generative decoder', default=True)
+    parser.add_argument('--cols', type=str, nargs='+', help='certain cols from the data files as the input features')
+    parser.add_argument('--num_workers', type=int, default=0, help='data loader num workers')
+    parser.add_argument('--itr', type=int, default=2, help='experiments times')
+    parser.add_argument('--train_epochs', type=int, default=6, help='train epochs')
+    parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
+    parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
+    parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
+    parser.add_argument('--des', type=str, default='test',help='exp description')
+    parser.add_argument('--loss', type=str, default='mse',help='loss function')
+    parser.add_argument('--lradj', type=str, default='type1',help='adjust learning rate')
+    parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
+    parser.add_argument('--inverse', action='store_true', help='inverse output data', default=False)
+
+    parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
+    parser.add_argument('--gpu', type=int, default=0, help='gpu')
+    parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
+    parser.add_argument('--devices', type=str, default='0,1,2,3',help='device ids of multile gpus')
+
+    args = dotdict(vars(parser.parse_args()))
+    return args
+
+def Autoformer_default_args():
+    """Autoformer default arguments"""
+    parser = argparse.ArgumentParser(description='Autoformer & Transformer family for Time Series Forecasting')
+
+    # basic config
+    parser.add_argument('--is_training', type=int, default=1, help='status')
+    parser.add_argument('--model_id', type=str, default='test', help='model id')
+    parser.add_argument('--model', type=str, default='Autoformer',
+                        help='model name, options: [Autoformer, Informer, Transformer]')
+
+    # data loader
+    parser.add_argument('--data', type=str, default='custom', help='dataset type')
+    parser.add_argument('--root_path', type=str, default='./dataset/', help='root path of the data file')
+    parser.add_argument('--data_path', type=str, default='Inflation.csv', help='data file')
+    parser.add_argument('--features', type=str, default='M',
+                        help='forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, '
+                                'S:univariate predict univariate, MS:multivariate predict univariate')
+    parser.add_argument('--target', type=str, default='Austria', help='target feature in S or MS task')
+    parser.add_argument('--freq', type=str, default='m',
+                        help='freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, '
+                                'b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h')
+    parser.add_argument('--detail_freq', type=str, default='m', help='like freq, but use in predict')
+    parser.add_argument('--checkpoints', type=str, default='./Autoformer_checkpoints/', help='location of model checkpoints')
+
+    # forecasting task
+    parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
+    parser.add_argument('--label_len', type=int, default=48, help='start token length')
+    parser.add_argument('--pred_len', type=int, default=96, help='prediction sequence length')
+
+    # model define
+    parser.add_argument('--bucket_size', type=int, default=4, help='for Reformer')
+    parser.add_argument('--n_hashes', type=int, default=4, help='for Reformer')
+    parser.add_argument('--enc_in', type=int, default=92, help='encoder input size')
+    parser.add_argument('--dec_in', type=int, default=92, help='decoder input size')
+    parser.add_argument('--c_out', type=int, default=92, help='output size')
+    parser.add_argument('--d_model', type=int, default=512, help='dimension of model')
+    parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
+    parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
+    parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
+    parser.add_argument('--d_ff', type=int, default=2048, help='dimension of fcn')
+    parser.add_argument('--moving_avg', type=int, default=25, help='window size of moving average')
+    parser.add_argument('--factor', type=int, default=3, help='attn factor')
+    parser.add_argument('--distil', action='store_false',
+                        help='whether to use distilling in encoder, using this argument means not using distilling',
+                        default=True)
+    parser.add_argument('--dropout', type=float, default=0.05, help='dropout')
+    parser.add_argument('--embed', type=str, default='timeF',
+                        help='time features encoding, options:[timeF, fixed, learned]')
+    parser.add_argument('--activation', type=str, default='gelu', help='activation')
+    parser.add_argument('--output_attention', action='store_true', help='whether to output attention in encoder')
+    parser.add_argument('--do_predict', action='store_true', help='whether to predict unseen future data')
+
+    # optimization
+    parser.add_argument('--num_workers', type=int, default=10, help='data loader num workers')
+    parser.add_argument('--itr', type=int, default=2, help='experiments times')
+    parser.add_argument('--train_epochs', type=int, default=10, help='train epochs')
+    parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
+    parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
+    parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
+    parser.add_argument('--des', type=str, default='test', help='exp description')
+    parser.add_argument('--loss', type=str, default='mse', help='loss function')
+    parser.add_argument('--lradj', type=str, default='type1', help='adjust learning rate')
+    parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
+
+    # GPU
+    parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
+    parser.add_argument('--gpu', type=int, default=0, help='gpu')
+    parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
+    parser.add_argument('--devices', type=str, default='0,1,2,3', help='device ids of multile gpus')
+
+    args = dotdict(vars(parser.parse_args()))
+    return args
+
+if __name__ == '__main__':
+    args = Autoformer_default_args()
+    print(args)
+    FEDFormer_default_args()
+    Informer_default_args()
