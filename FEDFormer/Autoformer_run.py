1f7b0e8cd55ae8d948da19f61735cf423f24865b
OleSahlholdt <o.sahlholdt@gmail.com>
2025-08-06 13:25:32 +0200

    Fix naming

--- FEDFormer/Autoformer_run.py
+++ FEDFormer/Autoformer_run.py
@@ -122,4 +122,5 @@ if __name__ == "__main__":
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     seq_lengths = [6, 12, 24]
     kernel_sizes = [5, 9, 13]
-    run_experiment(args, seq_lengths, kernel_sizes, task_id="Autoformer_h1")
+    horizon = f"h{args.pred_len}"
+    run_experiment(args, seq_lengths, kernel_sizes, task_id=f"Autoformer_{horizon}")
